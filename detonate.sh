2621f3845c902c8242f9c766db0dfd0efa573dab
bclin8 <bclin@andrew.cmu.edu>
2022-05-04 08:59:39 -0700

    Updated code to branch bryan

---
+++ detonate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm /mnt/shady_stuff/Documents/pic.jpg
+sync
+7z e /mnt/shady_stuff/Documents/zbsm.zip -o/mnt/shady_stuff/Documents/
+sync
+rm /mnt/shady_stuff/Documents/0
+rm /mnt/shady_stuff/Documents/1
+rm /mnt/shady_stuff/Documents/2
+rm /mnt/shady_stuff/Documents/zbsm.zip
+sync
